de7c6a5229f9b8d9d08c276a55b153a0fe0ee554
Salsa <vova.goncharenko@ekreative.com>
2015-11-23 15:55:51 +0200

    Tests

---
+++ src/AppBundle/Tests/Controller/TrainerControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CommandControllerTest extends WebTestCase
+{
+    /**
+     * @param $url
+     * @param $code
+     * @dataProvider testIndexProvider
+     */
+    public function testIndex($url, $code)
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+        $this->assertEquals($code, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexProvider()
+    {
+        return [
+            ['/trainer', 404],
+            ['/player', 404],
+        ];
+    }
+}
