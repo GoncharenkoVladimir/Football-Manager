51ea473aafe876ac14811fa80446fc66552217e9
Salsa <vova.goncharenko@ekreative.com>
2015-11-30 10:54:41 +0200

    Testing

--- src/AppBundle/Tests/Controller/PlayerControllerTest.php
+++ src/AppBundle/Tests/Controller/PlayerControllerTest.php
@@ -12,8 +12,6 @@ class PlayerControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/player/1');
 
 
-        $this->assertContains(
-            'Vasiliy Pupkin',
-            $crawler->filter('baner h3')->text());
+        $this->assertContains('Vasiliy Pupkin', $crawler->filter('.baner h3')->text());
     }
 }
